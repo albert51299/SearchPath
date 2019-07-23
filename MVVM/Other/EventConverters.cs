7add5b2f95c7bcdcbcb881f9d8a4d635640e9487
Albert <albert51299@gmail.com>
2019-07-23 20:22:08 +0500

    Add folders

---
+++ MVVM/Other/EventConverters.cs
@@ -0,0 +1,23 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MVVM.Other {
+    public class MouseButtonEventArgsToPointConverter : IEventArgsConverter {
+        private double nodeSize = 30;
+        public object Convert(object value, object parameter) {
+            var args = (MouseEventArgs)value;
+            var element = (FrameworkElement)parameter;
+            Point realPoint = args.GetPosition(element);
+            Point addNodePoint = realPoint;
+            addNodePoint.X -= nodeSize / 2;
+            addNodePoint.Y -= nodeSize / 2;
+            return new List<Point> { realPoint, addNodePoint };
+        }
+    }
+}
--- MVVM/View/MainWindow.xaml.cs
+++ MVVM/View/MainWindow.xaml.cs
@@ -24,17 +24,4 @@ namespace MVVM {
             DataContext = new AppViewModel(new ShowResultDialog());
         }
     }
-
-    public class MouseButtonEventArgsToPointConverter : IEventArgsConverter {
-        private double nodeSize = 30;
-        public object Convert(object value, object parameter) {
-            var args = (MouseEventArgs)value;
-            var element = (FrameworkElement)parameter;
-            Point realPoint = args.GetPosition(element);
-            Point addNodePoint = realPoint;
-            addNodePoint.X -= nodeSize / 2;
-            addNodePoint.Y -= nodeSize / 2;
-            return new List<Point> { realPoint, addNodePoint };
-        }
-    }
 }
